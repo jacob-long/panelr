94f579b8fc19a149176208429a5cef343bed0ce0
Jacob Long <jlongrc@gmail.com>
2018-03-25 23:12:05 -0400

    Document wbm class

--- R/1_wbMod.R
+++ R/1_wbMod.R
@@ -1,5 +1,15 @@
 #### wbMod class ##################################
 ## S4 declaration of wbMod superclass 
+#' @title Within-Between Model (`wbm`) class
+#' @details Models fit using [wbm()] return values of this class, which
+#'  inherits from \code{\link[lme4]{merMod-class}}. 
+#' @slot call_info A list of metadata about the arguments used.
+#' @slot call The actual function call.
+#' @slot summ The [jtools::summ()] object returned from calling it on the
+#'  `merMod` object.
+#' @slot summ_atts The attributes of the `summ` object.
+#' @slot orig_data The data provided to the `data` argument in the function 
+#'  call.
 #' @export
 setClass("wbm",
     # contains = "merMod",
