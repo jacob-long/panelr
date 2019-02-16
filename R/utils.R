3aa8c8cf7aad8110b5c0bf225b290ef1963a0fb5
Jacob Long <jlongrc@gmail.com>
2019-02-15 19:29:44 -0500

    De-duplicate utility functions from jtools

--- R/utils.R
+++ R/utils.R
@@ -6,40 +6,12 @@ magrittr::`%>%`
 #' @export
 magrittr::`%<>%`
 
-# Opposite of %in%
-`%nin%` <- function(x, table) {
-  is.na(match(x, table, nomatch = NA_integer_))
-}
-
-# Automate the addition of newline characters for long strings
-wrap_str <- function(..., sep = "") {
-  paste0(strwrap(paste(..., sep = sep), width = 0.95 * getOption("width", 80)),
-         collapse = "\n")
-}
-
-# Go ahead and wrap the cat function too
-cat_wrap <- function(..., brk = "") {
-  cat(wrap_str(...), brk, sep = "")
-}
-
-# Like cat_wrap but for warnings
-warn_wrap <- function(..., call. = FALSE, brk = "\n") {
-  warning(wrap_str(...), brk, call. = call.)
-}
-
-# Like cat_wrap but for errors
-stop_wrap <- function(..., call. = FALSE, brk = "\n") {
-  stop(wrap_str(...), brk, call. = call.)
-}
-
-# Like cat_wrap but for messages
-msg_wrap <- function(..., brk = "\n") {
-  message(wrap_str(...), brk)
-}
-
 backtick_name <- function(x) {
   if (make.names(x) != x) {paste0("`", x, "`")} else {x}
 }
+
+#' @import jtools
+
 
 #' @importFrom lme4 isLMM
 #' @importFrom methods as
