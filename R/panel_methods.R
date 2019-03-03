f9a1016fefa9420a3b8f5fd2a6b29126ccf3a4ad
Jacob Long <jlongrc@gmail.com>
2019-03-03 08:54:23 -0500

    Fix up S3 meethod consistency and add plm to suggests

--- R/panel_methods.R
+++ R/panel_methods.R
@@ -1,37 +1,41 @@
+#' @rdname panel_data
 #' @export
 
-as_pdata.frame <- function(x) {
+as_pdata.frame <- function(data) {
   
   if (!requireNamespace("plm")) {
     stop_wrap("You must have the plm package to convert to pdata.frame.")
   }
   
-  pdata.frame(unpanel(x), index = c(get_id(x), get_wave(x)))
+  plm::pdata.frame(unpanel(data), index = c(get_id(data), get_wave(data)))
   
 }
 
 #' @rdname panel_data
 #' @export
-as_panel_data <- function(x, ...) {
+as_panel_data <- function(data, ...) {
   UseMethod("as_panel_data")
 }
 
 #' @rdname panel_data
 #' @export
-as_panel_data.default <- function(x, id = id, wave = wave, ...) {
-  panel_data(x, id = !! id, wave = !! wave, ...)
+as_panel_data.default <- function(data, id = id, wave = wave, ...) {
+  panel_data(data, id = !! id, wave = !! wave, ...)
 }
 
 #' @rdname panel_data
 #' @export
-as_panel_data.pdata.frame <- function(x, ...) {
-  indices <- plm::index(x)
+as_panel_data.pdata.frame <- function(data, ...) {
+  if (!requireNamespace("plm")) {
+    stop_wrap("You must have the plm package to convert from pdata.frame.")
+  }
+  indices <- plm::index(data)
   id <- names(indices)[1]
   wave <- names(indices)[2]
-  if (id %nin% names(x)) {
+  if (id %nin% names(data)) {
     x[id] <- indices[id]
   }
-  if (wave %nin% names(x)) {
+  if (wave %nin% names(data)) {
     x[wave] <- indices[wave]
   }
   panel_data(x, id = !! id, wave = !! wave, ...)
