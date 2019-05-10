fc032e3ce8d8f14f487c30e0ed62d5d832467c13
Jacob Long <jlongrc@gmail.com>
2019-05-10 11:26:39 -0400

    Fix up line_plot a little bit

--- R/plotters.R
+++ R/plotters.R
@@ -21,14 +21,24 @@
 #'  `add.mean` is TRUE. Default is `"lm"`, but another option of interest is
 #'  `"loess"`.
 #' @param line.size The thickness of the plotted lines. Default: 0.5
+#' @param alpha The transparency for the lines and points. When 
+#'   `overlay = TRUE`, it is set to 0.5, otherwise 1, which means 
+#'   non-transparent.
 #' @return The `ggplot` object.
+#' @examples
+#' 
+#' data("WageData")
+#' wages <- panel_data(WageData, id = id, wave = t)
+#' line_plot(wages, lwage, add.mean = TRUE, subset.ids = TRUE, overlay = FALSE)
+#' 
 #' @rdname line_plot
 #' @export 
 #' @importFrom ggplot2 facet_wrap ggplot geom_path geom_point geom_smooth aes
 line_plot <- function(data, var, id = NULL, wave = NULL, overlay = TRUE,
-                      show.points = TRUE,  subset.ids = NULL,
-                      n.random.subset = 8, add.mean = FALSE,
-                      mean.function = "lm", line.size = 0.5) {
+                      show.points = TRUE,  subset.ids = FALSE,
+                      n.random.subset = 9, add.mean = FALSE,
+                      mean.function = "lm", line.size = 1,
+                      alpha = if (overlay) 0.5 else 1) {
   
   if (is_panel(data)) {
     id <- get_id(data)
@@ -41,7 +51,7 @@ line_plot <- function(data, var, id = NULL, wave = NULL, overlay = TRUE,
   }
   
   odata <- data
-  if (!is.null(subset.ids)) {
+  if (all(subset.ids != FALSE)) {
     if (length(subset.ids) == 1 && subset.ids %in% c(TRUE, "random")) {
       subset.ids <- sample(unique(data[[id]]), size = n.random.subset)
     }
@@ -51,26 +61,31 @@ line_plot <- function(data, var, id = NULL, wave = NULL, overlay = TRUE,
   }
   
   p <- ggplot(data = data,
-              aes(x = !! sym(wave), y = !! enquo(var), group = !! sym(id) ))
+              aes(x = !! sym(wave), y = !! enquo(var), group = !! sym(id)))
   
-  if (show.points == TRUE) {
-    p <- p + geom_point()
-  }
-  
-  p <- p + geom_path(size = line.size)
-  
-  if (overlay == FALSE) {
-    p <- p + facet_wrap(as.formula(paste0("~", id)))
-  }
+  p <- p + geom_path(size = line.size, alpha = alpha)
   
   if (add.mean == TRUE & overlay == TRUE) {
     p <- p + geom_smooth(data = odata, aes(x = !!sym(wave), y = !! enquo(var)),
                          inherit.aes = FALSE, method = mean.function,
                          colour = "blue", se = FALSE)
+    if (all(subset.ids != FALSE)) {
+      msg_wrap("The mean line is for all entities, not just the subset.")
+    }
   } else if (add.mean == TRUE) {
-    warning("Cannot add mean when `overlay` is FALSE.")
+    p <- p + geom_smooth(data = data, method = mean.function,
+                         colour = "blue", se = FALSE, size = line.size)
+  }
+  
+  if (show.points == TRUE) {
+    p <- p + geom_point(alpha = alpha)
+  }
+  
+  if (overlay == FALSE) {
+    p <- p + facet_wrap(as.formula(paste0("~", id)))
   }
   
   p + jtools::theme_nice(legend.pos = "right")
   
 }
+
